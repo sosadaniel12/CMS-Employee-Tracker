9b3c6ee88aabd8ddac1ffb5ba05d4f83a7af8a9d
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-10-11 21:47:32 -0500

    added the schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,24 @@
-DROP DATABASE IF EXISTS employee_db
-CREATE DATABASE employee_db
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
 
+USE employeeTracker_db;
+
+CREATE TABLE department(
+id INT NOT NULL,
+name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+id INT PRIMARY KEY NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+id INT PRIMARY KEY NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT
+);
