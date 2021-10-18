3b86bfcaa21c779cb305fbc353be860641b7dde5
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-10-18 10:01:33 -0500

    set up the connection to the database

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 
 //Ask the question for the user
-
+inquirer.prompt([]);
 
 //connect to the database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Tigrez13!",
+  database: "employeeTracker_db",
+});
 //Query database
+
